d93e29a80d0403537d849a2fe947bd77560e6e88
osborndasiagbonor01-ctrl <osborndasiagbonor01@gmail.com>
2026-08-01 17:38:55 +0000

    Initial upload of SalesRecorderV3 project
    Updated the app to run on a specified port and hostUploaded the complete SalesRecorderV3 web application developed with Python, Flask, HTML, and CSS. The project records sales, calculates totals, displays sales records, and allows deleting entries..

--- app.py
+++ app.py
@@ -65,5 +65,5 @@ def edit(index):
 
     return render_template("edit.html", sale=sale, index=index)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
